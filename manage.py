32eaf4a8077762ad8ae8626f057eb1aa256a13b0
John Benedict Solomon <johnbenedict.solomon@g.msuiit.edu.ph>
2019-03-26 15:50:48 +0800

    Initialezed API Blueprint

--- manage.py
+++ manage.py
@@ -8,6 +8,7 @@ from app.main.model import user
 from app.main import create_app, db
 
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+app.register_blueprint(blueprint)
 
 app.app_context().push()
 
