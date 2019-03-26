5e5623bf8d14850b70575d320cef1525542c19fa
John Benedict Solomon <johnbenedict.solomon@g.msuiit.edu.ph>
2019-03-26 16:00:26 +0800

    Added Blacklist table

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import unittest
 
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-from app.main.model import user
+from app.main.model import user, blacklist
 
 from app import blueprint
 
